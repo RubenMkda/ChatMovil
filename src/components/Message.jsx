3600e86135695787054db67eb3c6fe2d6f3e9067
RubenMkda <Moncadaruben22@gmail.com>
2023-03-16 18:29:43 -0400

    chat messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,13 +1,13 @@
 const Message = () => {
     return(
-        <div className="message">
+        <div className="message owner">
             <div className="messageinfo">
                 <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSAN0j6rMWinaEoUZRjc-1rYDAtKa09Jc6UJbyMHCy07KuKM7sJ" alt="" className="icon"/>
                 <span>Just Now</span>
             </div>
             <div className="messagecontent">
                 <p>Hello</p>
-                {/*<img src="https://i.blogs.es/755972/berserk-manga-2/840_560.jpeg" alt="" />*/}
+                <img src="https://i.blogs.es/755972/berserk-manga-2/840_560.jpeg" alt="" />
             </div>
         </div>
     )
